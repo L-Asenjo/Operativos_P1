4786b396093c7a2e4b5f4a00208b9fe22bc07f07
L-Asenjo <156253241+L-Asenjo@users.noreply.github.com>
2025-10-24 23:15:53 -0400

    Add files via upload

---
+++ SistemasOperativos-Proyecto1/src/main/main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import Interfaz.Interface1;
+import EDD.Cola;
+import EDD.Lista;
+import EDD.Proceso;
+import EDD.PCB;
+
+/**
+ *
+ * @author miche_ysmoa6e
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Interface1 menu = new Interface1();
+        menu.setVisible(true);
+        //menu.runQuickAddDemo();
+        
+        //exampleListaSwap();
+        //exampleColaSwap();
+        // TODO code application logic here
+    }
+    
+    // Example A: use Lista.swap directly with Proceso objects
+    
+}
